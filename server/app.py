56bd624d2e81ba008b43bd3608894e24186c4ec9
czaraugustocoder <cesaraugustoandradeferreira@gmail.com>
2024-09-04 00:08:03 +0000

    inserção de uma lógica ao formulário

--- server/app.py
+++ server/app.py
@@ -1,10 +1,17 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def register():
+
+    if request.method == "POST":
+
+        name = request.form['name']
+        email = request.form['email']
+        pwd = request.form['password']
+        return "Your name is "+name
     return render_template("register.html")
 
 @app.route("/labirinto")
