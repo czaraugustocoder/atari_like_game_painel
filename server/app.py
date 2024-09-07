fb19566f2d3fe380017dd3cb4721f9c104dd8d00
czaraugustocoder <cesaraugustoandradeferreira@gmail.com>
2024-09-07 13:55:17 -0400

    import do modulo sheetconn no app

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template, request, redirect, url_for, session
 from markupsafe import escape
+from sheetconn import SheetConn
 
 app = Flask(__name__)
 
